3f8c7d6569c1fd4dc3165da42177e379edcd978c
B. Tarun Kumar <btarunkumar7010@gmail.com>
2025-11-28 18:54:55 +0530

    Autowired, Qualifier, etc in spring

---
+++ src/main/java/com/example/autowired/annotation/Manager.java
@@ -0,0 +1,26 @@
+package com.example.autowired.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Manager {
+//    Field Injection
+    @Autowired
+    @Qualifier("employee")
+    private Employee employee;
+
+//    Constructor Injection
+//    @Autowired
+//    public Manager(Employee employee){
+//        this.employee = employee;
+//    }
+
+    @Override
+    public String toString() {
+        return "Manager{" +
+                "employee=" + employee +
+                '}';
+    }
+}
