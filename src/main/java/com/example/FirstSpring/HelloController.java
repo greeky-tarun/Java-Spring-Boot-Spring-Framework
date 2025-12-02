2346c0eda1cb84c855fc4b4a042040be6fdd2482
B. Tarun Kumar <btarunkumar7010@gmail.com>
2025-12-02 17:33:15 +0530

    REST api with get and post method

--- src/main/java/com/example/FirstSpring/HelloController.java
+++ src/main/java/com/example/FirstSpring/HelloController.java
@@ -1,14 +1,22 @@
 package com.example.FirstSpring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 // ready to use spring mvc to handle web request, It combination of controller + response body
 @RestController
 public class HelloController {
+    @GetMapping("/hello/{name}")
+    public HelloResponse helloParam(@PathVariable String name){
+        return new HelloResponse("Hello, "+name);
+    }
 
     @GetMapping("/hello")
-    public String Hello(){
-        return "Hello world";
+    public HelloResponse Hello(){
+        return new HelloResponse("Hello world!");
+    }
+
+    @PostMapping("/hello")
+    public HelloResponse helloPost(@RequestBody String name){
+        return new HelloResponse("Hello " + name + "!");
     }
 }
